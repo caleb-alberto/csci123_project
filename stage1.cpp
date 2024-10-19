2e6f42bc4af6a094246d332c8b49a8192bdabd90
caleb-alberto <calberto3418@gmail.com>
2024-10-19 10:19:17 -0700

    stage 1 (no descriptions) complete

--- stage1.cpp
+++ stage1.cpp
@@ -13,19 +13,16 @@ void look(std::string noun, std::string object) {
     }
 }
 
-bool parseinput(std::string action, bool objInInventory) {
-    std::string verb = action.substr(0, action.find(' '));
-    std::string noun = action.substr(action.find(' ')+1);
+bool parseinput(std::string verb, std::string noun, bool objInInventory) {
     std::string object = "OBJECT";
     
     if (verb == "look") {
-        std::cout << "string noun:" << noun << std::endl;
         look(noun, object);
     }
     else if (verb == "get" && noun == object) {
         if (objInInventory == false) {
             std::cout << "you have picked up " << object << std::endl;
-            return true;
+            objInInventory = true;
         }
         else {
             std::cout << "there is nothing to pick up" << std::endl;
@@ -34,11 +31,14 @@ bool parseinput(std::string action, bool objInInventory) {
     else if (verb == "drop" && noun == object) {
         if (objInInventory == true) {
             std::cout << "you have dropped " << object << std::endl;
-            return false;
+            objInInventory = false;
         }
         else {
             std::cout << "you have nothing to drop" << std::endl;
         }
+    }
+    else if (verb == "inventory") {
+        (objInInventory == true) ? std::cout << object << std::endl : std::cout << "nothing" << std::endl;
     }
     else if (verb == "go") {
         std::cout << "you can't go that way" << std::endl;
@@ -46,32 +46,23 @@ bool parseinput(std::string action, bool objInInventory) {
     else {
         std::cout << "you can't do that" << std::endl;
     }
+    return objInInventory;
 }
 
 int main() {
-    std::cout << "> ";
-    std::string action;
-    getline(std::cin, action);
-    std::string verb = action.substr(0, action.find(' '));
     bool objInInventory = false; 
-
-    if (verb == "quit") {
-        return 0;
-    } else {
-        objInInventory = parseinput(action, objInInventory);
-        parseinput(std::string action, bool objInInventory)
-    }
 
     do {
         std::cout << "> ";
         std::string action;
         getline(std::cin, action);
         std::string verb = action.substr(0, action.find(' '));
+        std::string noun = action.substr(action.find(' ')+1);
 
         if (verb == "quit") {
             break;
         } else {
-            parseinput(action, objInInventory);
+            objInInventory = parseinput(verb, noun, objInInventory);
         }
     }
     while (std::cin);
